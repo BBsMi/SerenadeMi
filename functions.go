5ddd77d57b863c55bf459409839a9ac44ddd834c
Stephanie Sunshine <ponyosunshine@gmail.com>
2023-05-17 19:56:19 -0700

    Verifing Init() is working as expected.

--- functions.go
+++ functions.go
@@ -6,4 +6,14 @@ import (
 
 func Init(i *Inits) {
   fmt.Println(i)
+  t := reflect.TypeOf(&i)
+  fmt.Println(t.NumMethod())
+
+  for m := 0; m < t.NumMethod(); m++ {
+    // fmt.Println(runtime.FuncForPC(t.Method(m).Pointer()).Name())
+    mm := t.Method(m)
+    fmt.Println(mm.Name)
+    val := reflect.ValueOf(&i).MethodByName(mm.Name).Call([]reflect.Value{})
+    fmt.Println(val)
+  }
 }
